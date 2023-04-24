3e096db2b4f894f2fcbab74870456bc399fa3f73
meliodas137 <mayankgt1043@gmail.com>
2023-04-24 01:43:17 -0400

    printing usage information for the api

--- nyufile.c
+++ nyufile.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char *filename = NULL, *sha1 = NULL;
+// char *disk;
+
+void showUsage(){
+    printf("Usage: ./nyufile disk <options>\n");
+    printf("  -i                     Print the file system information.\n");
+    printf("  -l                     List the root directory.\n");
+    printf("  -r filename [-s sha1]  Recover a contiguous file.\n");
+    printf("  -R filename -s sha1    Recover a possibly non-contiguous file.\n");
+    exit(0);
+}
 
 int main(int argc, char* argv[]){
 
