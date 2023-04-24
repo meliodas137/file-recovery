051ccd618726f07a7b1dab2a9cd622c2b35ec0f5
meliodas137 <mayankgt1043@gmail.com>
2023-04-24 01:43:39 -0400

    validaing cmd args

--- nyufile.c
+++ nyufile.c
@@ -14,6 +14,52 @@ void showUsage(){
     exit(0);
 }
 
-int main(int argc, char* argv[]){
-
+int operation(int argc, char* argv[]) {
+    opterr = 0;
+    int ch, op = 0;
+    while ((ch = getopt(argc, argv, "ilr:R:s:")) != -1) {
+        switch (ch) {
+            case 'i': 
+                if(op == 0) op = 1;
+                else showUsage();
+                break;
+            case 'l': 
+                if(op == 0) op = 2; 
+                else showUsage();
+                break;
+            case 'r': 
+                if(filename == NULL && (op == 0 || op == 5)) {
+                    op = 3;
+                    if((filename = (char*)optarg) == NULL) showUsage();
+                }
+                else showUsage();
+                break;
+            case 'R': 
+                if(filename == NULL && (op == 0 || op == 5)) {
+                    op = 4;
+                    if((filename = (char*)optarg) == NULL) showUsage();
+                }
+                else showUsage();
+                break;
+            case 's':
+                if(sha1 == NULL && (op == 0 || op == 3 || op == 4)) {
+                    if(op == 0) op = 5;
+                    if((sha1 = (char*)optarg) == NULL) showUsage();
+                }
+                else showUsage();
+                break;
+            default: showUsage();
+        }
+    }
+    if(optind != argc-1 || op == 5) showUsage();
+    else if(sha1 != NULL) op = 4;
+    // disk = (char*)argv[optind];
+    return op;
 }
+
+int main(int argc, char* argv[]){
+    operation(argc, argv);
+    return 0;
+}
+
+// docker run -i --name file-recovery --privileged --rm -t -v "C:\Users\mayan\Desktop\Git Repo\Sem 2\file-recovery:/file-recovery" -w /file-recovery ytang/os bash
